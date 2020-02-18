f4be52419f33f0ccf68af2f85e49e545e3806a13
Isla Hoe <ihoe@zendesk.com>
2020-02-18 15:41:34 +0000

    no idea what i'm doing

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "subnet_ids" {
-  type    = list(string)
-  default = ["subnet-0554e7fcce71e4a41", "subnet-072725ac1e98e80da", "subnet-08acede2e4c5e0fb3", "subnet-0bde161adafc24cb7"]
-}
-
-locals {
-  service_name = "forum"
-  owner        = "Community Team"
-}
